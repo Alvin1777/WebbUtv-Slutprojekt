dbccda5adc48e3ff5efe93f6b918b7b48a9a110c
Alvin1777 <alvin.soderberg@skola.taby.se>
2023-05-11 21:48:22 +0200

    pagetransitions med javascript

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
 
-
-
-// function validateForm() {
-//     // Hämtar värdena från formuläret
-//     let email = document.getElementById('email').value;
-
-//     let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     if (!emailPattern.test(email)) {
-//       alert('Vänligen ange en korrekt e-postadress.');
-//       return false;
-//     }
-// }
-
-
-//Navigation 
+// Navigation 
 function toggleMenu(){
     const toggleMenu = document.querySelector(".toggleMenu")
     const navigation = document.querySelector(".navigation")
@@ -21,3 +7,34 @@ function toggleMenu(){
     navigation.classList.toggle("active")
 }
 // End Navigation
+
+
+
+
+// Page Transition
+
+window.onload = () => {
+    const transition_element = document.querySelector('.transition')
+    const anchors = document.querySelectorAll('a')
+
+    setTimeout( () => {
+         transition_element.classList.remove('is-active')
+    }, 500);
+
+    for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            let target = e.target.href;
+
+            transition_element.classList.add('is-active');
+
+            setTimeout(() => {
+                window.location.href = target
+            }, 500);
+        })
+    }
+}
+
+// End Page Transition
